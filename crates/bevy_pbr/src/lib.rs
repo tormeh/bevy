9c6415cac3f01b797ef403d7c4cb69a406938d92
Chris Biscardi <chris@christopherbiscardi.com>
2026-02-06 21:17:52 -0800

    remove Debug from material strings and add LoadContext to GltfExtensi… (#22844)
    # Objective
    
    https://github.com/bevyengine/bevy/pull/22569 included some regressions.
    At least some of them are due to the debug formatting in the asset
    names.
    
    This results in asset names like: `"DefaultMaterial"#std`.
    
    ## Solution
    
    Remove the debug formatting.
    
    Additionally, enable the initialization of resources like the
    `StandardMaterial` built from a `GltfMaterial::default()` so that the
    asset loader will have access to them. Materials like this default
    material that don't exist will not call the `on_material` hook, and thus
    not be initialized to be referenced later.
    
    ## Testing
    
    multi_asset_sync and hot_reloading are fixed by this.
    
    <img width="3776" height="2096" alt="screenshot-2026-02-06-at-19 11
    31@2x"
    src="https://github.com/user-attachments/assets/07a31bf9-4013-4e80-a285-9b078eb28875"
    />
    <img width="3776" height="2096" alt="screenshot-2026-02-06-at-19 16
    42@2x"
    src="https://github.com/user-attachments/assets/5f1c1848-9469-4108-9600-5115d71d7e4c"
    />
    
    but anisotropy still looks wrong, so this is a fix for one issue and
    there appears to be more to do.
    
    <img width="3776" height="2096" alt="screenshot-2026-02-06-at-19 17
    20@2x"
    src="https://github.com/user-attachments/assets/70bd1fe2-39e4-439d-a88e-e34b39aa06a8"
    />

--- crates/bevy_pbr/src/lib.rs
+++ crates/bevy_pbr/src/lib.rs
@@ -28,7 +28,10 @@ mod atmosphere;
 mod cluster;
 mod components;
 pub mod contact_shadows;
-use bevy_gltf::extensions::{GltfExtensionHandler, GltfExtensionHandlers};
+use bevy_gltf::{
+    extensions::{GltfExtensionHandler, GltfExtensionHandlers},
+    GltfAssetLabel,
+};
 use bevy_render::sync_component::SyncComponent;
 pub use contact_shadows::{
     ContactShadows, ContactShadowsBuffer, ContactShadowsPlugin, ContactShadowsUniform,
@@ -453,6 +456,16 @@ impl GltfExtensionHandler for GltfExtensionHandlerPbr {
     fn dyn_clone(&self) -> Box<dyn GltfExtensionHandler> {
         Box::new((*self).clone())
     }
+    fn on_root(&mut self, load_context: &mut LoadContext<'_>, _gltf: &gltf::Gltf) {
+        // create the `StandardMaterial` for the glTF `DefaultMaterial` so
+        // it can be accessed when meshes don't have materials.
+        let std_label = format!("{}#std", GltfAssetLabel::DefaultMaterial);
+
+        load_context.add_labeled_asset(
+            std_label,
+            standard_material_from_gltf_material(&GltfMaterial::default()),
+        );
+    }
 
     fn on_material(
         &mut self,
@@ -462,11 +475,12 @@ impl GltfExtensionHandler for GltfExtensionHandlerPbr {
         material_asset: &GltfMaterial,
         material_label: &str,
     ) {
-        let std_label = format!("{:?}#std", material_label);
+        let std_label = format!("{}#std", material_label);
 
-        let _t = load_context.labeled_asset_scope::<_, ()>(std_label, |_load_context| {
-            Ok(standard_material_from_gltf_material(material_asset))
-        });
+        load_context.add_labeled_asset(
+            std_label,
+            standard_material_from_gltf_material(material_asset),
+        );
     }
 
     fn on_spawn_mesh_and_material(
@@ -478,7 +492,7 @@ impl GltfExtensionHandler for GltfExtensionHandlerPbr {
         entity: &mut EntityWorldMut,
         material_label: &str,
     ) {
-        let std_label = format!("{:?}#std", material_label);
+        let std_label = format!("{}#std", material_label);
         let handle = load_context.get_label_handle::<StandardMaterial>(std_label);
 
         entity.insert(MeshMaterial3d(handle));
